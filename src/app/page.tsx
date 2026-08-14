ff38bc48996c8c5efa6c61f8e33d48e634dcc39e
Audrin Luc <dylanblck811@gmail.com>
2026-08-14 17:03:26 +0200

    added things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Experience } from '@/components/Tour/Experience';
 import { Footer } from '@/components/Footer/Footer';
+import { Coda } from '@/components/Footer/Coda';
 
 export default function Home() {
   return (
     <main id="tour" className="relative bg-void">
       <Experience />
       <Footer />
+      <Coda />
     </main>
   );
 }
