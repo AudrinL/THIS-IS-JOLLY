038a55fe63571a4b9081493dcdcff0ffe2338eee
Audrin Luc <dylanblck811@gmail.com>
2026-08-14 17:39:39 +0200

    fixed things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import { Experience } from '@/components/Tour/Experience';
 import { Footer } from '@/components/Footer/Footer';
-import { Coda } from '@/components/Footer/Coda';
 
 export default function Home() {
   return (
     <main id="tour" className="relative bg-void">
       <Experience />
       <Footer />
-      <Coda />
     </main>
   );
 }
